b3a06f61a76a1d3c1e228890410871c2eb912567
Kris Armstrong <kris.armstrong@me.com>
2024-11-22 14:47:12 -0500

    styling fixes
    fixes for nav menu
    fixes for mobile menu
    fixes for touch

--- menu.js
+++ menu.js
@@ -4,5 +4,5 @@ const menu = document.querySelector('nav ul');
 
 // Add click event to toggle the menu visibility
 toggleButton.addEventListener('click', () => {
-    menu.classList.toggle('show'); // Add or remove the "show" class
+    menu.classList.toggle('open'); // Add or remove the "show" class
 });
