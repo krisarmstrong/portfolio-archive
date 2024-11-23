747ef75fcf645ff622720bff255e022bc034a96b
Kris Armstrong <kris.armstrong@me.com>
2024-11-22 20:58:54 -0500

    navbar still not working

--- menu.js
+++ menu.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     const toggleButton = document.querySelector(".menu-toggle");
-    const menu = document.querySelector("nav ul.menu");
+    const menu = document.querySelector("nav ul");
 
     if (toggleButton && menu) {
         toggleButton.addEventListener("click", () => {
